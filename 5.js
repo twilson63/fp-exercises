091e6a51b4c33fe61c6eafec08898df71e617ded
Tom Wilson <tom@jackhq.com>
2017-06-06 11:32:16 -0400

    add filter

---
+++ 5.js
@@ -0,0 +1,4 @@
+const utils = require('./fp-utils')
+
+// filters
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
--- fp-utils.js
+++ fp-utils.js
@@ -1,14 +1,18 @@
 module.exports = {
   forEach: forEach,
-  map: map
+  map: map,
+  filter: filter
 }
 
 // pure functions
-
-function forEach(fn, list) {
-  for (let i = 0; i < list.length; i++) {
-    fn(list[i])
-  }
+function filter(fn, list) {
+  let results = []
+  forEach(function(v) {
+    if (fn(v)) {
+      results.push(v)
+    }
+  }, list)
+  return results
 }
 
 function map(fn, list) {
@@ -18,4 +22,10 @@ function map(fn, list) {
     results.push(result)
   }, list)
   return results
+}
+
+function forEach(fn, list) {
+  for (let i = 0; i < list.length; i++) {
+    fn(list[i])
+  }
 }
